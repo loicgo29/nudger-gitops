32928566674734dcf7709cdfd153de3e379d49a3
LoGo <loicgourmelon@gmail.com>
2025-09-16 04:14:54 +0000

    fix: autocommit

--- scripts/autom/3fluxReconcileKustomizations.sh
+++ scripts/autom/3fluxReconcileKustomizations.sh
@@ -8,8 +8,7 @@ flux get kustomizations -A | tail -n +2 | awk '{print $1 "/" $2}' | grep "infra-
   ns=$(echo $item | cut -d/ -f1)
   name=$(echo $item | cut -d/ -f2)
   [[ -z "$ns" || -z "$name" ]] && continue
-  echo "🔄 flux reconcile kustomization $name -n $ns --with-source"
-  flux reconcile kustomization $name -n $ns --with-source
+  flux reconcile kustomization "$name" -n "$ns" --with-source
 done
 
 # 2. Reconcile des autres
@@ -17,6 +16,5 @@ flux get kustomizations -A | tail -n +2 | awk '{print $1 "/" $2}' | grep -v "inf
   ns=$(echo $item | cut -d/ -f1)
   name=$(echo $item | cut -d/ -f2)
   [[ -z "$ns" || -z "$name" ]] && continue
-  echo "🔄 flux reconcile kustomization $name -n $ns --with-source"
-  flux reconcile kustomization $name -n $ns --with-source
+  flux reconcile kustomization "$name" -n "$ns" --with-source
 done
