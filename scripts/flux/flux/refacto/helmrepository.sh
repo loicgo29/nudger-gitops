30958538234099b657bedb79ae0c2cfb6a49237f
LoGo <loicgourmelon@gmail.com>
2025-09-16 06:06:58 +0000

    fix: autocommit

--- scripts/flux/flux/refacto/helmrepository.sh
+++
@@ -1,4 +0,0 @@
- cat 1 | cut -d':' -f1 | sort -u|while read file; do  echo -n "$file: "; yq '.metadata.name' "$file"; done >
- find ./infra -name '*helmrelease.yaml' | while read file; do
-  echo -n "$file: "; yq '.metadata.name' "$file"
-done > helmrelease-liist.txt
