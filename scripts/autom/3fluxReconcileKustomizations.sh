274dda47d5d840086ace3940a31d36eca31ec610
LoGo <loicgourmelon@gmail.com>
2025-09-23 18:12:52 +0000

    chore: auto-commit [20250923-181252]

--- scripts/autom/3fluxReconcileKustomizations.sh
+++ scripts/autom/3fluxReconcileKustomizations.sh
@@ -8,18 +8,19 @@ run() {
 
 echo "🔄 Reconciling all Kustomizations (namespaces first)"
 
-# 1. Reconcile des Kustomizations 'infra-namespaces'
-flux get kustomizations -A | tail -n +2 | awk '{print $1 "/" $2}' | grep "infra-namespaces" | while read item; do
+# Récupération des Kustomizations
+kustomizations=$(flux get kustomizations -A | tail -n +2 | awk '{print $1 "/" $2}')
+
+# 1. Forcer l’ordre : namespaces d’abord
+for item in $(echo "$kustomizations" | grep "infra-namespaces"); do
   ns=$(echo $item | cut -d/ -f1)
   name=$(echo $item | cut -d/ -f2)
-  [[ -z "$ns" || -z "$name" ]] && continue
   run flux reconcile kustomization "$name" -n "$ns" --with-source
 done
 
-# 2. Reconcile des autres
-flux get kustomizations -A | tail -n +2 | awk '{print $1 "/" $2}' | grep -v "infra-namespaces" | while read item; do
+# 2. Puis le reste
+for item in $(echo "$kustomizations" | grep -v "infra-namespaces"); do
   ns=$(echo $item | cut -d/ -f1)
   name=$(echo $item | cut -d/ -f2)
-  [[ -z "$ns" || -z "$name" ]] && continue
   run flux reconcile kustomization "$name" -n "$ns" --with-source
 done
